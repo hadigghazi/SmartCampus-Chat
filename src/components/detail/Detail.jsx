52831bcf9a7993d75da7a0af6f17bf5df6a654af
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-10 16:37:33 +0300

    Finalizing detail component

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,40 +1,38 @@
 import { useState } from "react";
-import "./detail.css";
+import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
 import { useChatStore } from "../../lib/chatStore";
 import { auth, db } from "../../lib/firebase";
 import { useUserStore } from "../../lib/userStore";
-import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
-
-
-const handleLogout = () => {
-  auth.signOut();
-  resetChat();
-};
-
-const handleBlock = async () => {
-  if (!user) return;
-
-  const userDocRef = doc(db, "users", currentUser.id);
-
-  try {
-    await updateDoc(userDocRef, {
-      blocked: isReceiverBlocked ? arrayRemove(user.id) : arrayUnion(user.id),
-    });
-    changeBlock();
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const { chatId, user, isCurrentUserBlocked, isReceiverBlocked, changeBlock, resetChat } =
-  useChatStore();
-const { currentUser } = useUserStore();
+import "./detail.css";
 
 const Detail = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); 
+  const { chatId, user, isCurrentUserBlocked, isReceiverBlocked, changeBlock, resetChat } =
+    useChatStore();
+  const { currentUser } = useUserStore();
 
   const toggleDetail = () => {
     setIsOpen(!isOpen);
+  };
+
+  const handleBlock = async () => {
+    if (!user) return;
+
+    const userDocRef = doc(db, "users", currentUser.id);
+
+    try {
+      await updateDoc(userDocRef, {
+        blocked: isReceiverBlocked ? arrayRemove(user.id) : arrayUnion(user.id),
+      });
+      changeBlock();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleLogout = () => {
+    auth.signOut();
+    resetChat();
   };
 
   return (
@@ -62,7 +60,6 @@ const Detail = () => {
       </div>
     </>
   );
-  
 };
 
 export default Detail;
