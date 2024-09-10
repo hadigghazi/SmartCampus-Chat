667323d649eb2e8fd0ea1c7ed4f26f0c8d0406c0
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-10 16:01:29 +0300

    Adding avatar to the login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,6 +2,20 @@ import { useState } from "react";
 import "./login.css";
 
 const Login = () => {
+  const [avatar, setAvatar] = useState({
+    file: null,
+    url: "",
+  });
+  
+  const handleAvatar = (e) => {
+    if (e.target.files[0]) {
+      setAvatar({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0]),
+      });
+    }
+  };
+  
   return (
     <div className="login">
       <div className="item">
@@ -19,6 +33,16 @@ const Login = () => {
           <input type="text" placeholder="Username" name="username" />
           <input type="text" placeholder="Email" name="email" />
           <input type="password" placeholder="Password" name="password" />
+          <label htmlFor="file">
+      <img src={avatar.url || "./avatar.png"} alt="" />
+      Upload an image
+    </label>
+    <input
+      type="file"
+      id="file"
+      style={{ display: "none" }}
+      onChange={handleAvatar}
+    />
           <button>Sign Up</button>
         </form>
       </div>
