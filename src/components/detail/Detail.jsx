e41f6fc81294bb3c16a95ad5abe70ae6f091041c
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-10 16:34:53 +0300

    Adding handle block function

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -3,6 +3,22 @@ import "./detail.css";
 import { useChatStore } from "../../lib/chatStore";
 import { auth, db } from "../../lib/firebase";
 import { useUserStore } from "../../lib/userStore";
+import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
+
+const handleBlock = async () => {
+  if (!user) return;
+
+  const userDocRef = doc(db, "users", currentUser.id);
+
+  try {
+    await updateDoc(userDocRef, {
+      blocked: isReceiverBlocked ? arrayRemove(user.id) : arrayUnion(user.id),
+    });
+    changeBlock();
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const { chatId, user, isCurrentUserBlocked, isReceiverBlocked, changeBlock, resetChat } =
   useChatStore();
