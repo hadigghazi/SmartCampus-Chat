b587e08f78145b3609cd4308a838c135557134a6
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-10 17:32:39 +0300

    Initializing add user component

---
+++ src/components/list/chatList/addUser/addUser.jsx
@@ -0,0 +1,26 @@
+import "./addUser.css";
+import { useState } from "react";
+
+const AddUser = () => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <div className="addUser">
+      <form>
+        <input type="text" placeholder="Username" name="username" />
+        <button>Search</button>
+      </form>
+      {user && (
+        <div className="user">
+          <div className="detail">
+            <img src="./avatar.png" alt="" />
+            <span>{user.username}</span>
+          </div>
+          <button>Add User</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AddUser;
