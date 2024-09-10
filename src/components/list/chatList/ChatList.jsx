21f04c5bbcbe93f913dc2a3813ee8ce7a753da06
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-10 17:28:17 +0300

    Initializing chat list component

---
+++ src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import "./chatList.css";
+import AddUser from "./addUser/addUser";
+import { useUserStore } from "../../../lib/userStore";
+import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
+import { db } from "../../../lib/firebase";
+import { useChatStore } from "../../../lib/chatStore";
+
+const ChatList = () => {
+  const [chats, setChats] = useState([]);
+  const [addMode, setAddMode] = useState(false);
+  const [input, setInput] = useState("");
+
+  const { currentUser } = useUserStore();
+  const { chatId, changeChat } = useChatStore();
+
+  return (
+    <div className="chatList">
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="" />
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+      {addMode && <AddUser />}
+    </div>
+  );
+};
+
+export default ChatList;
