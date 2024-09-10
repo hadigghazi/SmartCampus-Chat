0dd3d2a2f8dd21fbf7a77abb3520f4447ec36881
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-10 16:34:26 +0300

    Adding state manager hooks for detail

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
 import "./detail.css";
+import { useChatStore } from "../../lib/chatStore";
+import { auth, db } from "../../lib/firebase";
+import { useUserStore } from "../../lib/userStore";
+
+const { chatId, user, isCurrentUserBlocked, isReceiverBlocked, changeBlock, resetChat } =
+  useChatStore();
+const { currentUser } = useUserStore();
 
 const Detail = () => {
   const [isOpen, setIsOpen] = useState(false);
