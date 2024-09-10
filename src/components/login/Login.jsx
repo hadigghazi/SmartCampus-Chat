0e7e0e3c72e696007febea26c8e2c143bcf57569
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-10 16:00:26 +0300

    Initializing setup of the Login component with basic structure

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./login.css";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="item">
+        <h2>Welcome back,</h2>
+        <form>
+          <input type="text" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button>Sign In</button>
+        </form>
+      </div>
+      <div className="separator"></div>
+      <div className="item">
+        <h2>Create an Account</h2>
+        <form>
+          <input type="text" placeholder="Username" name="username" />
+          <input type="text" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
