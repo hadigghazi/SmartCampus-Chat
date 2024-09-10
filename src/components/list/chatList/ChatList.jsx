e2faae454fa1c4d4ab7f849342510876f28ab8cb
Hadi Ghazi <hadigghazi@gmail.com>
2024-09-10 17:30:51 +0300

    Finalizing chat ist component

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -19,36 +19,60 @@ const ChatList = () => {
       doc(db, "userchats", currentUser.id),
       async (res) => {
         const items = res.data().chats;
-  
+
         const promises = items.map(async (item) => {
           const userDocRef = doc(db, "users", item.receiverId);
           const userDocSnap = await getDoc(userDocRef);
-  
+
           const user = userDocSnap.data();
-  
+
           return { ...item, user };
         });
-  
+
         const chatData = await Promise.all(promises);
-  
+
         setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
       }
     );
-  
+
     return () => {
       unSub();
     };
   }, [currentUser.id]);
-  
+
+  const handleSelect = async (chat) => {
+    const userChats = chats.map((item) => {
+      const { user, ...rest } = item;
+      return rest;
+    });
+
+    const chatIndex = userChats.findIndex(
+      (item) => item.chatId === chat.chatId
+    );
+
+    userChats[chatIndex].isSeen = true;
+
+    const userChatsRef = doc(db, "userchats", currentUser.id);
+
+    try {
+      await updateDoc(userChatsRef, {
+        chats: userChats,
+      });
+      changeChat(chat.chatId, chat.user);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   const filteredChats = chats.filter((c) =>
     c.user?.username.toLowerCase().includes(input.toLowerCase())
   );
-  
+
   return (
     <div className="chatList">
       <div className="search">
         <div className="searchBar">
-          <img src="./search.png" alt="Search" />
+          <img src="./search.png" alt="" />
           <input
             type="text"
             placeholder="Search"
@@ -57,7 +81,7 @@ const ChatList = () => {
         </div>
         <img
           src={addMode ? "./minus.png" : "./plus.png"}
-          alt={addMode ? "Close" : "Add"}
+          alt=""
           className="add"
           onClick={() => setAddMode((prev) => !prev)}
         />
@@ -92,7 +116,6 @@ const ChatList = () => {
       {addMode && <AddUser />}
     </div>
   );
-  
 };
 
 export default ChatList;
